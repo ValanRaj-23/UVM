8b129417b41ba8fb17f76873b9cfa13ef3b1e3c9
VALAN RAJ S <valanrajs.2364@gmail.com>
2025-06-06 23:36:23 +0530

    Update 05_BASE_MONITOR.sv

--- UVM_UART_TX/TESTBENCH/05_BASE_MONITOR.sv
+++ UVM_UART_TX/TESTBENCH/05_BASE_MONITOR.sv
@@ -1,113 +1,77 @@
 class base_mon extends uvm_monitor;
-  `uvm_component_utils(base_mon)
+	`uvm_component_utils(base_mon)
+	base_seq_item		m_item	;
+  	virtual base_intf 	vinf;
+	uvm_analysis_port#(base_seq_item) item_tx;
+	
+
+
+
+	function new(string name = "base_mon", uvm_component parent);
+		super.new(name, parent);
+      		m_item = new();
+	endfunction
+
+
+
+	function void build_phase(uvm_phase phase);
+		super.build_phase(phase);
+		item_tx = new("item_tx", this);
+      
+      if(!uvm_config_db#(virtual base_intf)::get(this, " ", "vinf", vinf))
+        `uvm_fatal("VIRTUAL INTERFACE", "NOT AVAILABLE")
+      
+	endfunction
+
+
+
+	task run_phase(uvm_phase phase);
+		super.run_phase(phase);
+            `uvm_info(get_type_name(), "RUN_PHASE STARTED", UVM_LOW)
+      drive_to_scb();
+	endtask
+
+	
+  task drive_to_scb();
+		forever begin
+          start_condition();        
+		end   
+  endtask
+
   
-  			bit 		d_start		;
-  			bit			d_stop		;
-  			bit			d_data		;
-  			bit 		p_bit		;
   
-  
-  virtual base_intf vinf;
-  base_seq_item seq_item;
-  uvm_analysis_port# (base_seq_item) item_producer;
-    
-  
-  
-  
-  function new(string name = "base_mon", parent);
-    super.new(name, parent);
-    `uvm_info(get_type_name, "CONSTRUCTOR", UVM_DEBUG)
-  endfunction
-  
-  
-  
-  
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-		`uvm_info(get_type_name(), "BUILD_PHASE"	, UVM_FULL)
-    if(!uvm_config_db#(virtual base_intf)::get(this, "", "vinf", vinf))
-       `uvm_fatal("Can't retrive", " virtual interface")
-      item_producer = new("item_producer", this);
-    seq_item = new();
-  endfunction
-    
-       
-  
-  
-  task run_phase(uvm_phase phase);
+  task start_condition();
     forever begin
-      wait(vinf.reset)
-      start_condition();
-      slave_address();
-      parity_condition();
-      stop_condition();
-      d_start = 0;
-    end
-  endtask
-  
-  
-	task start_condition();
-    	wait(vinf.reset)
-      @(negedge vinf.baud_wire)
-    		if(!vinf.tx_out)
-              begin
-              	`uvm_info(get_type_name(), "START_CONDITION DETECTED", UVM_MEDIUM)
-                d_start = 1;
-                seq_item.input_data = vinf.data;
-              end
-    		else 
-              begin
-        		`uvm_info(get_type_name(), "STOP_CONDITION DETECTED", UVM_MEDIUM)
-        		break;
-      		end
-   
-  endtask
-  
-  
-  task slave_address();
-    if(seq_item.d_start || vinf.baud_wire)
-      for(int i = 0; i <= 7; i++)
+      @(posedge vinf.baud_wire)
+      if(vinf.tx_out == 0)
         begin
-        @(negedge vinf.baud_wire)
-          seq_item.uart_data[i] = vinf.tx_out;
-          d_start = 1;
-        end
-    
-    seq_item.uart_data_q = seq_item.uart_data;
-    d_data = 1;
-    break;
-  endtask
-  
-  
-  
-  task parity_condition();
-    if(d_start)
-      @(negedge vinf.baud_wire)
-      if(vinf.p_sel)
-        seq_item.p_bit = ^seq_item.uart_data;
-    	else
-          seq_item.p_bit = ~(^seq_item.uart_data);
-      	break;
-    
-  endtask
-  
-  
-  
-  task stop_condition();
-    if(d_start)
-      @(negedge vinf.baud_wire)
-      if(vinf.baud_wire)
-        begin
-        seq_item.d_stop = 1;
-          `uvm_info(get_type_name(), "STOP_CONDITION DETECTED", UVM_MEDIUM)
-        end
-    else 
-      begin
-        `uvm_info(get_type_name(), "STOP_CONDITION DETECTED", UVM_MEDIUM)
-        break;
+          `uvm_info(get_type_name(), "START CONDITION DETECTED", UVM_MEDIUM)
+          		
+          	m_item.data 	<= vinf.data;
+          `uvm_info(get_type_name(), $sformatf("input_data = %0d", vinf.data), UVM_MEDIUM)
+
+          
+	for(int i = 0; i <= 8; i++)
+            begin
+    	    @(posedge vinf.baud_wire)
+            m_item.tx_data[i]	= vinf.tx_out;
+            end
+             
+          
+          `uvm_info(get_type_name(), $sformatf("uart_data = %0d", m_item.tx_data), UVM_MEDIUM)
+
+          
+            @(posedge vinf.baud_wire)
+          	begin
+            if(vinf.tx_out == 1)
+              `uvm_info(get_type_name(), "STOP CONDITION DETECTED", UVM_MEDIUM)
+			end
+             
+              item_tx.write(m_item);
+		end
+	else
+      break;
       end
-    item_producer.write(seq_item);
-  endtask
-    	    
-  
+endtask
+       
 endclass

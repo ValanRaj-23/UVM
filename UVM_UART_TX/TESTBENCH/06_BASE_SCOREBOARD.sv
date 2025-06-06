6cbc3defb8bb9a5b12cef8080395226407d87cf1
VALAN RAJ S <valanrajs.2364@gmail.com>
2025-06-06 23:36:47 +0530

    Update 06_BASE_SCOREBOARD.sv

--- UVM_UART_TX/TESTBENCH/06_BASE_SCOREBOARD.sv
+++ UVM_UART_TX/TESTBENCH/06_BASE_SCOREBOARD.sv
@@ -1,53 +1,60 @@
 class base_scb extends uvm_scoreboard;
-  `uvm_component_utils(base_scb)
-  bit [7:0]	scb_input_data[$];
-  bit [7:0] scb_output_data[$];
-  base_seq_item	seq_item_q[$];
-  base_seq_item scb_item;
-  int q_size;
-  event trans_done;--------------
-  
-  uvm_analysis_imp #(base_seq_item, base_scb) item_receiver;
-  
-  
-  
-  function new(string name = "base_scb", uvm_component parent);
-    super.new(name, parent);
-    `uvm_info(get_type_name, "CONSTRUCTOR", UVM_DEBUG)
-  endfunction
-  
-  
-      
-  
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-		`uvm_info(get_type_name(), "BUILD_PHASE"	, UVM_FULL)
-    item_receiver = new("item_receiver", this);
-  endfunction
-  
-  
-  
-  
-  fuction void write(base_seq_item seq_item);
-  seq_item_q.push_back(seq_item);
-  `uvm_info(get_type_name(), "WRITE_FUNCTION", UVM_MEDIUM)
-  endfunction
-  
+	`uvm_component_utils(base_scb)
+  	uvm_analysis_imp#(base_seq_item, base_scb) item_rx;
+  	base_seq_item 	scb_q[$];
+  	base_seq_item   scb_item;
+	uvm_event sb_done;
   
   
 
-  task run(uvm_phase phase)
-    base_seq_item seq_item;
-    forever begin
-    if(seq_item_q.size(); != 0)
-        scb_item 	= seq_item_q.pop_front();
-      if(scb_item.uart_data  == scb_item.data)
-        `uvm_info(get_type_name(), $sformatf(" PASSED INPUT_DATA = %0d | UART_DATA = %0d:",scb_item.data, scb_item.uart_data) UVM_MEDIUM)
-        else
-        `uvm_info(get_type_name(), $sformatf(" FAILED INPUT_DATA = %0d | UART_DATA = %0d:",scb_item.data, scb_item.uart_data) UVM_MEDIUM)
-    end
-   --> trans_done;
-  endtask
-   
+	function new(string name = "base_scb", uvm_component parent);
+		super.new(name, parent);
+            `uvm_info(get_type_name(), "Constructor", UVM_LOW)
+      scb_item = new();
+	endfunction
+
+
+
+	function void build_phase(uvm_phase phase);
+		super.build_phase(phase);
+		item_rx = new("item_rx", this);
+
+      if(!uvm_config_db#(uvm_event)::get(this, " ", "sb_done_id", sb_done))
+             `uvm_fatal("GET_EVT", "Cannot get sb_done_evt")
+	endfunction
+
+	
+  
+  function void write(base_seq_item dummy);
+    scb_q.push_front(dummy);
+  endfunction
+
+  
+  
+  task run_phase(uvm_phase phase);
+		super.run_phase(phase);
+          `uvm_info(get_type_name(), "RUN_PHASE STARTED", UVM_LOW)
+    	check_output();
+	endtask
+
+
+  
+  
+ 	task check_output();
+		forever begin
+          wait(scb_q.size() > 0);
+          scb_item = scb_q.pop_back();
+          
+          if(scb_item.data == scb_item.tx_data)
+            `uvm_info(get_type_name(), "CASE PASSED", UVM_LOW)
+          else
+            `uvm_error(get_type_name(), "CASE FAILED")
+            sb_done.trigger();
+          
+		end
+	endtask
+
+            
+            
 endclass
 
